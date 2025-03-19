9df321a2dce67c615ec23b7e0962a3262deee500
smoben <bsmolcz@gmail.com>
2025-03-19 21:18:57 +0100

    update with project1

--- Project1/wiki/encyclopedia/urls.py
+++ Project1/wiki/encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry_page, name="entry_page")
+    path("wiki/<str:title>", views.entry_page, name="entry_page"),
+    path("search/", views.search, name="search"),
+    path("new_page/", views.new_page, name="new_page")
 ]
--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -1,15 +1,75 @@
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from . import util
 
 
+class SearchForm(forms.Form):
+    search_ = forms.CharField(label="Search Entries")
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": SearchForm()
     })
 
 def entry_page(request, title):
     return render(request, "encyclopedia/entry_page.html", {
         "titles": util.get_entry(title),
-        "page_title": title
+        "page_title": title,
+        "form": SearchForm()
     })
+
+def search(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search_ = form.cleaned_data["search_"]
+            if util.get_entry(search_):
+                return render(request, "encyclopedia/search.html", {
+                    "form": form,
+                    "search_": search_,
+                    "titles": util.get_entry(search_),
+                    "page_title": search_,
+                })
+            else:
+                entry_list = []
+                for i in util.list_entries():
+                        if search_.capitalize() in i.capitalize():
+                            entry_list.append(i)
+                return render(request, "encyclopedia/search.html", {
+                    "form": form,
+                    "search_": search_,
+                    "titles": util.get_entry(search_),
+                    "page_title": search_,
+                    "entries": util.list_entries(),
+                    "entry_list": entry_list
+                })
+        else:
+            return render(request, "encyclopedia/search.html", {
+                "form": form
+            })
+
+def new_page(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        if len(title)>1 and len(content)>1:
+            util.save_entry(title, content)
+            return render(request, "encyclopedia/new_page.html", {
+                "entries": util.list_entries(),
+                "form": SearchForm()
+            })
+        else:
+            return render(request, "encyclopedia/new_page.html", {
+                "entries": util.list_entries(),
+                "form": SearchForm()
+            })
+    else:
+        return render(request, "encyclopedia/new_page.html", {
+            "entries": util.list_entries(),
+            "form": SearchForm()
+        })
+    
