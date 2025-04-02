92e05edaabc4920a8084e27e9aba4c2a6cac9878
smoben <bsmolcz@gmail.com>
2025-04-02 18:23:34 +0200

    Project 2 first version

--- Project2/commerce/auctions/models.py
+++ Project2/commerce/auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListings(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=100)
+    bid = models.IntegerField()
+    pic_url = models.URLField()
+
+    def __str__(self):
+        return f"{self.id}: {self.title}"
+
+class Bids(models.Model):
+    pass
+
+class Comments(models.Model):
+    pass
--- Project2/commerce/auctions/views.py
+++ Project2/commerce/auctions/views.py
@@ -4,11 +4,21 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from django import forms
+from .models import User, AuctionListings
+
+
+class CreateForm(forms.Form):
+    title = forms.CharField(label="Title of your auction", max_length=64)
+    description = forms.CharField(widget=forms.Textarea, label="Your decription", max_length=100)
+    bid = forms.IntegerField(label="Starting bid")
+    pic_url = forms.URLField(label="Picture link (optional)", required=False)
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html", {
+        "auctions": AuctionListings.objects.all()
+    })
 
 
 def login_view(request):
@@ -61,3 +71,24 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+    
+def create(request):
+    if request.method == "POST":
+        form = CreateForm(request.POST)
+        if form.is_valid():
+            title1 = form.cleaned_data["title"]
+            description1 = form.cleaned_data["description"]
+            bid1 = form.cleaned_data["bid"]
+            pic_url1 = form.cleaned_data["pic_url"]
+            a = AuctionListings(title=title1, description=description1, bid=bid1, pic_url=pic_url1)
+            a.save()
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/create.html", {
+        "form": CreateForm()
+    })
+
+def listing(request):
+    return render(request, 'auctions/listing.html')
